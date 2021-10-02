d19d5eaea55f580a5c6c6f6b5c3d6844f6b015ca
wikiwaleed2 <wikiwaleed2@gmail.com>
2021-10-02 21:46:02 +0500

    Signed-off-by: wikiwaleed2 <wikiwaleed2@gmail.com>

--- nodeApp/server.js
+++ nodeApp/server.js
@@ -24,5 +24,5 @@ app.use(errorHandler);
 
 // start server
 const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
-app.listen(80);
+app.listen(port, () => { console.log("Started on port " + port); });
 
