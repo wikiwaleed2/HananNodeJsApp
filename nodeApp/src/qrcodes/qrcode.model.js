12c0dd4b6e81a46aa64d63f9b802775f68ddd773
wikiwaleed2 <wikiwaleed2@gmail.com>
2021-12-10 15:58:23 +0500

    Signed-off-by: wikiwaleed2 <wikiwaleed2@gmail.com>

--- nodeApp/src/qrcodes/qrcode.model.js
+++ nodeApp/src/qrcodes/qrcode.model.js
@@ -8,6 +8,7 @@ function model(sequelize) {
         hash: { type: DataTypes.STRING, allowNull: false }, 
         type: { type: DataTypes.STRING, allowNull: false }, // User or Admins
         url: { type: DataTypes.STRING, allowNull: false }, 
+        status: { type: DataTypes.STRING }, 
         created: { type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW },
         updated: { type: DataTypes.DATE },
     };
