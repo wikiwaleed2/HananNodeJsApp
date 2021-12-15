ddb419103dcb5d7b3f3177a6d5da45c6e39dfc30
wikiwaleed2 <wikiwaleed2@gmail.com>
2021-12-15 15:03:59 +0500

    Signed-off-by: wikiwaleed2 <wikiwaleed2@gmail.com>

--- nodeApp/src/pictures/picture.model.js
+++ nodeApp/src/pictures/picture.model.js
@@ -5,13 +5,14 @@ module.exports = model;
 function model(sequelize) {
     const attributes = {
         name: { type: DataTypes.STRING, allowNull: false },
-        description: { type: DataTypes.STRING, allowNull: false },
         url: { type: DataTypes.STRING, allowNull: false },
         alt: { type: DataTypes.STRING },
         type: { type: DataTypes.STRING, allowNull: false }, // thumbnail / normal / icon / banner etc...
         status: { type: DataTypes.STRING, allowNull: false }, // in-active etc / active / pending etc...
         format: { type: DataTypes.STRING },
-        for: { type: DataTypes.STRING },
+        category: { type: DataTypes.STRING },  // capmaign-image-gallery // charity-partner-image-gallery // charity-partner-desktop-imaage
+        platform: { type: DataTypes.STRING },  // mobile //desktop
+        description: { type: DataTypes.STRING, allowNull: false }, // name + type + status + format + category + platform 
 
         created: { type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW },
         updated: { type: DataTypes.DATE },
