16bb87ce1b7ec37506973e1c704884d0451ba3aa
wikiwaleed2 <wikiwaleed2@gmail.com>
2021-12-15 14:52:21 +0500

    Signed-off-by: wikiwaleed2 <wikiwaleed2@gmail.com>

--- nodeApp/src/pictures/picture.model.js
+++ nodeApp/src/pictures/picture.model.js
@@ -5,12 +5,13 @@ module.exports = model;
 function model(sequelize) {
     const attributes = {
         name: { type: DataTypes.STRING, allowNull: false },
-        title: { type: DataTypes.STRING, allowNull: false },
         description: { type: DataTypes.STRING, allowNull: false },
         url: { type: DataTypes.STRING, allowNull: false },
         alt: { type: DataTypes.STRING },
         type: { type: DataTypes.STRING, allowNull: false }, // thumbnail / normal / icon / banner etc...
         status: { type: DataTypes.STRING, allowNull: false }, // in-active etc / active / pending etc...
+        format: { type: DataTypes.STRING },
+        for: { type: DataTypes.STRING },
 
         created: { type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW },
         updated: { type: DataTypes.DATE },
